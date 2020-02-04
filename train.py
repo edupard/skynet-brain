df181ab45a4d1067d5ec4f8e39c6e7f8e239f87c
Evgeny Tarasov <tarasov.e.a@gmail.com>
2020-02-04 17:39:41 -0500

    test

--- train.py
+++ train.py
@@ -9,25 +9,14 @@ batch_size, timesteps, input_dim = None, 100, 10
 arr = np.load("batches_1.npy")
 x = arr[:,:,0:10]
 y = arr[:,99,10]
-# y = arr[:,99,20]
 
-# i = Input(batch_shape=(batch_size, timesteps, input_dim))
-#
-# o = TCN(return_sequences=False, dropout_rate=0.0, dilations=(1, 2, 4, 8, 16, 32, 64))(i)  # The TCN layers are here.
-# o = Dense(1)(o)
-#
-# m = Model(inputs=[i], outputs=[o])
-# m.compile(optimizer='adam', loss='mse')
-#
-# m.fit(x, y, epochs=1000, batch_size=100, validation_split=0.2)
+i = Input(batch_shape=(batch_size, timesteps, input_dim))
 
-i1 = Input(batch_shape=(batch_size, timesteps, input_dim))
+o = TCN(return_sequences=False, dropout_rate=0.5, dilations=(1, 2, 4, 8, 16, 32, 64))(i1)  # The TCN layers are here.
+o = Dense(1)(o)
 
-o1 = TCN(return_sequences=False, dropout_rate=0.5, dilations=(1, 2, 4, 8, 16, 32, 64))(i1)  # The TCN layers are here.
-o1 = Dense(1)(o1)
-
-m1 = Model(inputs=[i1], outputs=[o1])
+m1 = Model(inputs=[i], outputs=[o])
 m1.compile(optimizer='adam', loss='mse')
 
-m1.fit(x, y, epochs=30000, batch_size=100, validation_split=0.2)
+m1.fit(x, y, epochs=10, batch_size=100, validation_split=0.2)
 
